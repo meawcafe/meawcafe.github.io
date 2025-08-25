a8e53d992b983c3f9104c1067d58477d682debcd
Milton M <miltonfidelis21@hotmail.com>
2025-08-25 20:01:19 +0200

    feat(ui): add button to show 'What's New' modal

--- src/components/RightSidebar/index.jsx
+++ src/components/RightSidebar/index.jsx
@@ -3,11 +3,14 @@ import { useTouchStore } from "~/store/touch";
 import ViewsCounter from "./ViewsCounter";
 import { ArrowLeftIcon } from "@phosphor-icons/react";
 import ContactSection from "./Contact";
+import { InfoIcon } from "@phosphor-icons/react/dist/ssr";
+import { useWhatsNewStore } from "~/store/whatsNew";
 // import ThemeSwitch from "./ThemeSwitch";
 
 export default function RightSidebar() {
   const sectionIndex = useTouchStore((state) => state.sectionIndex);
   const { setSectionIndex } = useTouchStore.getState();
+  const { setIsModalOpen } = useWhatsNewStore.getState();
 
   const rightSidebarClasses = clsx(
     "right-sidebar-res w-[16rem] p-[3%] pl-0 flex flex-col gap-8",
@@ -38,6 +41,18 @@ export default function RightSidebar() {
       <ViewsCounter />
       {/* <ThemeSwitch /> */}
       <ContactSection />
+
+      {/* show whats new again */}
+      <div
+        className="anin-on-press gap-2 items-center text-[var(--text3)] hover:text-[var(--accent-color1)] cursor-pointer hover:bg-[var(--background1)] py-2 hover:pl-3 pr-4 rounded-full"
+        style={{
+          transition: "padding-left 0.2s, border 0.4s, background-color 0.2s",
+        }}
+        onClick={() => setIsModalOpen(true)}
+      >
+        <InfoIcon size={"1.5em"} />
+        <span className="text-base font-semibold">What's New</span>
+      </div>
     </div>
   );
 }
